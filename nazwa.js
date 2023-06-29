8103d8aa3996623133dcc44bee8abbf61ca5adde
Karolina Gorzolnik <137784165+Gorzolka@users.noreply.github.com>
2023-06-29 14:44:23 +0200

    dodaj plik nazwa.js

--- nazwa.js
+++ nazwa.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Talpanie w wodzie jest super');
+  res.end('Móóóóój jest ten kawałek kompuszka');
 }
 
 const server = http.createServer(requestListener);
